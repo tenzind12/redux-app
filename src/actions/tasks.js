7b7009d5ca0ddc30eb255dbd3dc5ad7cc4a8a1c5
Tenzin Dekyong <dhontenzin@gmail.com>
2022-04-02 17:58:13 +0200

    axios init

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,13 +1,13 @@
 import * as actionTypes from '../constants/action-types';
+import axios from 'axios';
 /* These functions are used by dispatch in Tasks.jsx to sent to STORE 
    which will be received by the REDUCER
 */
 export const fetchTasks = () => {
   return async (dispatch) => {
-    const response = await fetch('http://localhost:7000/tasks', { method: 'GET' });
-    const data = await response.json();
-    // console.log(data);
-    dispatch({ type: actionTypes.FETCH_TASKS, payload: data });
+    const response = await axios.get('http://localhost:7000/tasks');
+    console.log(response);
+    dispatch({ type: actionTypes.FETCH_TASKS, payload: response.data });
   };
 };
 
